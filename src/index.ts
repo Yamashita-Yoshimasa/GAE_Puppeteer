3284207973da1adb30eadadd6d00929722262a98
yamabaku <yychbook040720@gmail.com>
2021-11-19 01:09:23 +0900

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -50,33 +50,42 @@ export const OpenPage = async () => {
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+let namesResolve:  object | undefined;
+let pricesResolve:  object | undefined;
+let idResolve:  object | undefined;
+
+const GetData = async () => {
+  const Resolve = await OpenPage();
+  [namesResolve, pricesResolve, idResolve] = [
+    Resolve?.[0],
+    Resolve?.[1],
+    Resolve?.[2],
+  ];
+};
+
+GetData();
+
 app.get('/', (req, res) => {
   res.send(`Hello World port ${PORT}`)
 });
-// let namesResolve:  object | undefined;
-// let pricesResolve:  object | undefined;
-// let idResolve:  object | undefined;
 
-OpenPage().then((Response) => {
-  app.get('/pckoubou/names', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.json(JSON.stringify(Response?.[0]));
-  });
-  
-  app.get('/pckoubou/prices', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.json(JSON.stringify(Response?.[1]));
-  });
-  
-  app.get('/pckoubou/id', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.json(JSON.stringify(Response?.[2]));
-  });
-  
+
+app.get('/pckoubou/names', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.json(JSON.stringify(namesResolve));
 });
 
+app.get('/pckoubou/prices', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.json(JSON.stringify(pricesResolve));
+});
+
+app.get('/pckoubou/id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.json(JSON.stringify(idResolve));
+});
 
 app.listen(PORT, () => {
   console.log('Server running on port %d', PORT);
 });
-

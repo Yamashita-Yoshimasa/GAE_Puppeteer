a19a6a97ca729a7f559816489fa39e74d99bee1e
yamabaku <yychbook040720@gmail.com>
2021-11-18 21:46:00 +0900

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,49 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import express from 'express';
-import { OpenPage } from './containers/Puppeteer';
-import  { Test } from './containers/test'
+import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export const OpenPage = async () => {
+  let pckoubouNames: unknown = [];
+  let pckoubouPrices: unknown = [];
+  let pckoubouId: unknown = [];
+  dotenv.config();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+  });
+  const page = await browser.newPage();
+  await page.goto(process.env.PC_KOUBOU_URL || '', {
+    // ページを読み込むまで待機
+    waitUntil: 'networkidle2',
+    timeout: 0,
+  });
+  pckoubouNames = await page.$$eval('.name', (item) =>
+    item.map((names) => names.textContent),
+  );
+  pckoubouPrices = await page.$$eval('.price--num', (item) =>
+    item.map((prices) => prices.textContent),
+  );
+  pckoubouId = await page.$$eval('.item-code > dd', (item) =>
+    item.map((id) => id.textContent),
+  );
+
+  await browser.close();
+  // unknown型ガードl
+  if (
+    typeof pckoubouNames === 'object' &&
+    pckoubouNames != null &&
+    typeof pckoubouPrices === 'object' &&
+    pckoubouPrices != null &&
+    typeof pckoubouId === 'object' &&
+    pckoubouId != null
+  ) {
+    return Promise.resolve([pckoubouNames, pckoubouPrices, pckoubouId]);
+  }
+};
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -28,7 +69,7 @@ void GetData();
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.json(Test());
+  res.json(JSON.stringify(namesResolve));
 });
 
 app.get('/pckoubou/prices', (req, res) => {
